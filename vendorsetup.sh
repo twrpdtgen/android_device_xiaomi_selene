5a61d4c45c638216fc5ca0771b3647c81ce0408d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-04 16:28:44 +0100

    selene: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 21061119AG Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_selene-user
+add_lunch_combo omni_selene-userdebug
+add_lunch_combo omni_selene-eng
